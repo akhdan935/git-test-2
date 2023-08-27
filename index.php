3f3b57f0c512cfbd4e94cb0dafbf4de9213d1ce4
akhdan935 <ibrahimakhdaan935@gmail.com>
2023-08-27 11:58:00 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, ducimus cum. Repellat consequatur eaque ipsa aperiam eveniet praesentium necessitatibus illo iusto deleniti dolorem. Recusandae quae reprehenderit, neque dicta repudiandae aliquam!</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 
 </html>
